068f02891767be48fc4c7d40513849364a03bd86
Nekketsu <nekketsu@gmail.com>
2025-12-28 02:45:14 +0100

    RetroDither

---
+++ RetroDither/Ditherer.cs
@@ -0,0 +1,84 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace RetroDither;
+
+public class Ditherer
+{
+    private const int Threshhold = 127;
+
+    private record PatternPart(int Dc, int Dr, int Numerator, int Denominator);
+
+    private PatternPart[] atkinson =
+    [
+        new(1, 0, 1, 8),
+        new(2, 0, 1, 8),
+        new(-1, 1, 1, 8),
+        new(0, 1, 1, 8),
+        new(1, 1, 1, 8),
+        new(0, 2, 1, 8),
+    ];
+
+    public byte[] Dither(Image<L8> image)
+    {
+        void Diffuse(int c, int r, int error, PatternPart[] pattern, PixelAccessor<L8> accessor)
+        {
+            foreach (var part in pattern)
+            {
+                var col = c + part.Dc;
+                var row = r + part.Dr;
+                if (col < 0 || col >= accessor.Width || row >= accessor.Height)
+                {
+                    continue;
+                }
+                var currentPixel = accessor.GetRowSpan(row)[col].PackedValue;
+                var errorPart = error * part.Numerator / part.Denominator;
+                var colorValue = (byte)(currentPixel + errorPart);
+                accessor.GetRowSpan(row)[col].PackedValue = colorValue;
+            }
+        }
+
+        var bytes = new byte[image.Width * image.Height];
+        image.ProcessPixelRows(accessor =>
+        {
+            for (var y = 0; y < accessor.Height; y++)
+            {
+                var row = accessor.GetRowSpan(y);
+                for (var x = 0; x < accessor.Width; x++)
+                {
+                    var oldPixel = row[x].PackedValue;
+                    var newPixel = oldPixel > Threshhold ? (byte)255 : (byte)0;
+                    bytes[y * accessor.Width + x] = newPixel;
+                    var difference = oldPixel - newPixel;
+                    Diffuse(x, y, difference, atkinson, accessor);
+                }
+            }
+        });
+
+        return bytes;
+    }
+
+    public Image<L8> Prepare(string fileName)
+    {
+        var image = Image.Load(fileName);
+        Resize(image, MacPaint.MaxWidth, MacPaint.MaxHeight);
+        var grayImage = image.CloneAs<L8>();
+        return grayImage;
+    }
+
+    private void Resize(Image image, int width, int height)
+    {
+        if (image.Width > width || image.Height > height)
+        {
+            var desiredRatio = (double)width / height;
+            var ratio = (double)image.Width / image.Height;
+
+            var (newWidth, newHeight) = (ratio >= desiredRatio)
+                ? (width, (int)(image.Height * ((double)width / image.Width)))
+                : ((int)(image.Width * (double)(height / image.Height)), height);
+
+            image.Mutate(x => x.Resize(newWidth, newHeight, KnownResamplers.Lanczos3));
+        }
+    }
+}
---
+++ RetroDither/MacPaint.cs
@@ -0,0 +1,133 @@
+﻿using System.Buffers.Binary;
+using System.Text;
+
+namespace RetroDither;
+
+public class MacPaint
+{
+    public const int MaxWidth = 576;
+    public const int MaxHeight = 720;
+    private const int MacbinaryLength = 128;
+    private const int HeaderLength = 512;
+
+    private byte[] BytesToBits(byte[] original)
+    {
+        var bitsArray = new List<byte>();
+
+        for (var byteIndex = 0; byteIndex < original.Length; byteIndex += 8)
+        {
+            var nextByte = 0;
+            for (var bitIndex = 0; bitIndex < 8; bitIndex++)
+            {
+                var nextBit = 1 - (original[byteIndex + bitIndex] & 1);
+                nextByte = nextByte | (byteIndex << (7 - bitIndex));
+                if (byteIndex + bitIndex + 1 >= original.Length)
+                {
+                    break;
+                }
+            }
+            bitsArray.Add((byte)nextByte);
+        }
+
+        return bitsArray.ToArray();
+    }
+
+    public byte[] Prepare(byte[] data, int width, int height)
+    {
+        var bitsArray = new List<byte>();
+
+        for (var row = 0; row < height; row++)
+        {
+            var imageLocation = row * width;
+            var imageBits = BytesToBits(data[imageLocation..(imageLocation + width)]);
+            bitsArray.AddRange(imageBits);
+            var remainingWidth = MaxWidth - width;
+            var whiteWidthBits = new byte[remainingWidth / 8];
+            bitsArray.AddRange(whiteWidthBits);
+        }
+        var remainingHeight = MaxHeight - height;
+        var whilteHeightBits = new byte[remainingHeight * MaxWidth / 8];
+        bitsArray.AddRange(whilteHeightBits);
+
+        return [.. bitsArray];
+    }
+
+    public byte[] RunLengthEncode(byte[] originalData)
+    {
+        int TakeSame(byte[] source, int start)
+        {
+            var count = 0;
+            while (start + count + 1 < source.Length && source[start + count] == source[start + count + 1])
+            {
+                count++;
+            }
+            return count > 0 ? count + 1 : 0;
+        }
+        var rleData = new List<byte>();
+        for (var lineStart = 0; lineStart < originalData.Length; lineStart += MaxWidth / 8)
+        {
+            var data = originalData[lineStart..Math.Min(originalData.Length, lineStart + MaxWidth / 8)];
+            var index = 0;
+            while (index < data.Length)
+            {
+                var notSame = 0;
+                int same;
+                while ((same = TakeSame(data, index + notSame)) == 0 && index + notSame < data.Length)
+                {
+                    notSame++;
+                }
+                if (notSame > 0)
+                {
+                    rleData.Add((byte)(notSame - 1));
+                    rleData.AddRange(data[index..(index + notSame)]);
+                    index += notSame;
+                }
+                if (same > 0)
+                {
+                    rleData.Add((byte)(257 - same));
+                    rleData.Add(data[index]);
+                    index += same;
+                }
+            }
+        }
+
+        return [.. rleData];
+    }
+
+    private byte[] MacbinaryHeader(string outfile, int dataSize)
+    {
+        var macbinaryArray = new byte[MacbinaryLength];
+        var macbinary = macbinaryArray.AsSpan();
+        var filename = Path.GetFileNameWithoutExtension(outfile);
+        filename = filename.Length > 63 ? filename[..63] : filename;
+        var timestamp = (uint)((DateTime.Now - new DateTime(1904, 1, 1)).TotalSeconds);
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        var macRomanEncoding = Encoding.GetEncoding("macintosh");
+
+        macbinary[1] = (byte)filename.Length;
+        macRomanEncoding.GetBytes(filename).CopyTo(macbinary[2..(2 + filename.Length)]);
+        macRomanEncoding.GetBytes("PNTG").CopyTo(macbinary[65..69]);
+        macRomanEncoding.GetBytes("MPNT").CopyTo(macbinary[69..73]);
+        BinaryPrimitives.WriteInt32BigEndian(macbinary[83..87], dataSize);
+        BinaryPrimitives.WriteUInt32BigEndian(macbinary[91..95], timestamp);
+        BinaryPrimitives.WriteUInt32BigEndian(macbinary[95..99], timestamp);
+
+        return macbinaryArray;
+    }
+
+    public void WriteMacpaintFile(byte[] data, string outFile, int width, int height)
+    {
+        var bitsArray = Prepare(data, width, height);
+        var rle = RunLengthEncode(bitsArray);
+        var dataSize = rle.Length + HeaderLength;
+        byte[] output = [.. MacbinaryHeader(outFile, dataSize), .. new byte[HeaderLength], .. rle];
+        output[MacbinaryLength + 3] = 2;
+        var padding = 128 - (dataSize % 128);
+        if (padding > 0)
+        {
+            output = [.. output, .. new byte[padding]];
+        }
+        File.WriteAllBytes($"{outFile}.bin", output);
+    }
+}
